42363259ba0de48d45327c7dd79ece096fe032df
Vinicius Oliveira <viniciuswebdev@gmail.com>
2017-06-02 00:12:50 -0300

    Implement handler to publish on Mattermost

--- src/MattermostHandler.php
+++ src/MattermostHandler.php
@@ -2,18 +2,30 @@
 
 namespace MattermostHandler;
 
+use GuzzleHttp\Client;
 use Monolog\Logger;
 use Monolog\Handler\AbstractProcessingHandler;
 
 class MattermostHandler extends AbstractProcessingHandler
 {
-    public function __construct($level = Logger::DEBUG, $bubble = true)
+    private $webHookUrl;
+    private $client;
+
+    public function __construct($webHookUrl, $level = Logger::DEBUG, $bubble = true, $client = null)
     {
         parent::__construct($level, $bubble);
+
+        $this->webHookUrl = $webHookUrl;
+        $this->client = (!$client) ?: new Client();
     }
 
     protected function write(array $record)
     {
-
+        $client = new Client();
+        $client->request('POST', $this->webHookUrl, [
+            'form_params' => [
+                'payload' => json_encode(['text' => $record['formatted']])
+            ]
+        ]);
     }
 }
